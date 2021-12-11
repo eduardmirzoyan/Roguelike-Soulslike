be3de2c6f9c799f7bb79adeaeaadbc4ff0f09760
Eduard Mirzoyan <eduardmirzoyan123@gmail.com>
2021-12-11 01:11:47 -0800

    Tweaked Blocking
    Need to rework completely

--- Assets/PlayerShield.cs
+++ Assets/PlayerShield.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,20 +8,35 @@ public class PlayerShield : Shield
     [SerializeField] private Slider slider;
     [SerializeField] private GameObject sparkle;
 
+    [SerializeField] private InputBuffer inputBuffer;
+
     [SerializeField] private float perfectBlockTimeFrame = 0.5f;
     [SerializeField] private float perfectBlockTimer;
 
     [SerializeField] private int baseBlockDrain;
 
+    [SerializeField] private float blockTime;
+
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        inputBuffer = GetComponentInParent<InputBuffer>();
     }
 
     protected void FixedUpdate()
     {
         if (perfectBlockTimer > 0)
             perfectBlockTimer -= Time.deltaTime;
+
+        if(blockTime > 0)
+        {
+            GetComponentInParent<Movement>().dashWithVelocity(10, 1); // Fix this logic
+            blockTime -= Time.deltaTime;
+        }
+        else
+        {
+            GetComponentInParent<Movement>().dashWithVelocity(10, inputBuffer.moveDirection);
+        }
     }
 
     public override void blockDamage(Damage dmg)
@@ -33,18 +46,18 @@ public class PlayerShield : Shield
         // If player manages to block an attack during the perfect block time frame, then they don't take extra block stamina damage
         if (perfectBlockTimer > 0)
         {
-            takeStaminaDamage(0); // 0 extra drain 
+            takeBlockStaminaDamage(0); // 0 extra drain 
             Instantiate(sparkle, new Vector2(transform.position.x + 0.33f, transform.position.y + 0.25f), Quaternion.identity);
             GameEvents.current.triggerOnPerfectBlock(); // Trigger perfect block event for everyone who cares
         }
         else
-            takeStaminaDamage(dmg.damageAmount); // Drain stamina
+            takeBlockStaminaDamage(dmg.damageAmount); // Drain stamina
 
         // Knock holder back
-        GetComponentInParent<Rigidbody2D>().AddForce(dmg.origin * 4);
+        blockTime = 0.25f;
     }
 
-    private void takeStaminaDamage(int damage)
+    private void takeBlockStaminaDamage(int damage)
     {
         // Drains stamina and gets result about what to do to player
         damage -= GetComponentInParent<CombatStats>().defense / 4; // Reduce stamina damage by fourth of defense value
